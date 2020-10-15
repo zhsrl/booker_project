1fd85b93d7a90eb482726dae31626a8dd624fe50
Zhasaral Almabek <thunderccloud@gmail.com>
2020-10-15 06:30:13 +0600

    design and added Room

---
+++ app/src/main/java/com/e/booker/model/database/UserDatabase.kt
@@ -0,0 +1,24 @@
+package com.e.booker.model.database
+
+import androidx.room.Database
+import androidx.room.DatabaseConfiguration
+import androidx.room.InvalidationTracker
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+
+@Database(entities = arrayOf(UserEntity::class), version = 1)
+class UserDatabase: RoomDatabase() {
+
+
+    override fun createOpenHelper(config: DatabaseConfiguration?): SupportSQLiteOpenHelper {
+        TODO("Not yet implemented")
+    }
+
+    override fun createInvalidationTracker(): InvalidationTracker {
+        TODO("Not yet implemented")
+    }
+
+    override fun clearAllTables() {
+        TODO("Not yet implemented")
+    }
+}
